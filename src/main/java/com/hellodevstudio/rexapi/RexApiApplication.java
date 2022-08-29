416bc5ddaf4c4c55782ebdf75b8796feddeb4ba2
Carlos Galaviz <carlos@email.com>
2022-08-29 17:58:26 -0600

    Initial commit

---
+++ src/main/java/com/hellodevstudio/rexapi/RexApiApplication.java
@@ -0,0 +1,13 @@
+package com.hellodevstudio.rexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RexApiApplication.class, args);
+    }
+
+}
