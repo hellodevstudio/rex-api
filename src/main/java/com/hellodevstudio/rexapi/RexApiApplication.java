c4572cd85d60d62cf2c1700c4f27373c77b53166
Carlos Galaviz <carlos@email.com>
2022-08-30 18:10:25 -0600

    Refactored code

--- src/main/java/com/hellodevstudio/rexapi/RexApiApplication.java
+++ src/main/java/com/hellodevstudio/rexapi/RexApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RexApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RexApiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(RexApiApplication.class, args);
+  }
 
 }
